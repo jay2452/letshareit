6180c54b427bbc7e4462092f301d6e44b81e27ea
jay2452 <jay2452@live.com>
2015-09-11 17:54:19 +0530

    changes 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+	setTimeout(function(){
+		$('#notice_wrapper').fadeOut("slow", function() {
+			$(this).remove();
+		})
+	}, 4500);
+	
+	$('[data-toggle="tooltip"]').tooltip(); 	
+
+});
