af1df3e8ae7b492f08a5778c98aba4066b0076f4
jay2452 <jay2452@live.com>
2015-09-12 22:45:11 +0530

    uploads scaffold

---
+++ app/views/uploads/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @upload, :id, :topic, :user_id, :subject_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912144447) do
+ActiveRecord::Schema.define(version: 20150912171004) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20150912144447) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "uploads", force: :cascade do |t|
+    t.string   "topic"
+    t.integer  "user_id"
+    t.integer  "subject_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "uploads", ["subject_id"], name: "index_uploads_on_subject_id"
+  add_index "uploads", ["user_id"], name: "index_uploads_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
