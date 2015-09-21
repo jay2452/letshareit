45df69f17f405239dedb3486976be09ccacfe623
jay2452 <jay2452@live.com>
2015-09-21 12:15:14 +0530

    dd

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
+
+  protected
+
+  def after_update_path_for(resource)
+      root_path(resource)
+  end
+
   private
   def sign_up_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :branch_id)
