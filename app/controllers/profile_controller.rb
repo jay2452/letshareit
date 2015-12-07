3f723e341cdeb9bbcf5782b3647b4ce7b1716b07
jay2452 <jay2452@live.com>
2015-12-07 21:04:30 +0530

    ajax on user preferences added

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,9 +1,9 @@
 class ProfileController < ApplicationController
 	before_action :authenticate_user!
-
   def show
   	@user = current_user
   	@uploads = Upload.where(user_id: current_user.id).order(created_at: :desc)
   	@links = Link.where(user_id: current_user.id).order(created_at: :desc)
+  	@user_preference = UserPreference.new
   end
 end
