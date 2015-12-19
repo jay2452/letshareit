82d6ae7fd1cb3a136d270006fd6ca45a1878c0f9
amriteshchandan <amriteshchandan@yahoo.in>
2015-12-19 17:45:13 +0530

    Not SignedIn user home page

--- app/helpers/home_pages_helper.rb
+++ app/helpers/home_pages_helper.rb
@@ -11,4 +11,14 @@ module HomePagesHelper
 	def user_upload_preferences
 		@branch_subjects = Subject.where("branch_id = ?", current_user.branch_id)
 	end
+
+	def recent_uploads(branch)
+		subject_ids = Subject.where('branch_id = ?', branch.id).select(:id).map(&:id)
+		Upload.where("subject_id IN (?)", subject_ids).order(created_at: :desc).limit(7)
+	end
+
+	def recent_links(branch)
+		subject_ids = Subject.where('branch_id = ?', branch.id).select(:id).map(&:id)
+		Link.where("subject_id IN (?)", subject_ids).order(created_at: :desc).limit(7)
+	end
 end
