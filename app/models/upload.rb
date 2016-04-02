9bd5537ad056d68c927d002c645197ee99d2d0db
jay2452 <jay2452@live.com>
2016-04-02 23:58:04 +0530

    file suppoet extended

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -8,16 +8,18 @@ class Upload < ActiveRecord::Base
   validates :user_id, presence: true
   validates :subject_id, presence: true
   validates_attachment :file, presence: true
-  validates_attachment_content_type :file, content_type: %w(image/jpeg image/jpg image/png image/gif application/pdf
- 															application/msword
-                               application/mspowerpoint
-                               application/vnd.ms-powerpoint
-                               application/vnd.openxmlformats-officedocument.wordprocessingml.document
-                               application/vnd.openxmlformats-officedocument.presentationml.presentation
-                               application/msexcel
-                               application/vnd.ms-excel
-                               application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
-                               text/plain)
+  # validates_attachment_content_type :file, content_type: %w(image/jpeg image/jpg image/png image/gif application/pdf
+ # 															application/msword
+  #                              application/mspowerpoint
+  #                              application/vnd.ms-powerpoint
+  #                              application/vnd.openxmlformats-officedocument.wordprocessingml.document
+  #                              application/vnd.openxmlformats-officedocument.presentationml.presentation
+  #                              application/msexcel
+  #                              application/vnd.ms-excel
+  #                              application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+  #                              text/plain application/zip application/x-zip application/rar)
+
+  validates_attachment_content_type :file, not: %w(exe)
 
 	# searchable do
 	# 	text :topic, boost: 2
